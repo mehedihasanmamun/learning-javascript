03f1c5f23904b6e9a90130f2698dd990b57175f8
mehedihasanmamun <75701729+mehedihasanmamun@users.noreply.github.com>
2023-04-02 22:02:26 +0600

    Working on javascript day two

---
+++ day-2/index.js
@@ -0,0 +1,14 @@
+let firstName = 'Mehedi';
+let lastName = 'Mamun';
+let age = 25;
+
+
+let fullName = firstName  +     lastName;
+
+console.log(`My name is ${fullName}. I am ${age}.`)
+
+let js = 'javascript';
+console.log(js.length);
+
+let firstName1 = 'Mehedi Hasan Mamun'
+console.log(firstName1.length);
